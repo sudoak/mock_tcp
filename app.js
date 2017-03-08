cdd7cd39a9749d86414507b5198dbaa7d1ae5ad3
sudoak <sudoakk@gmail.com>
2017-03-08 17:18:46 +0530

    Command line usage

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ var net = require('net'),
 	random = require("random-js")(),
 	toFixed = require('tofixed')
 const commandLineArgs = require('command-line-args')
-	
+const getUsage = require('command-line-usage')
+
 const optionDefinitions = [
   { name: 'ip', alias: 'i', type: String },
   { name: 'port', alias: 'p', type: String},
   { name: 'message', alias: 'm', type: String}
 ]
+const usage = getUsage([
+  {
+    header: 'MOCK TCP',
+    content: 'Send data to a specific TCP ip|port'
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions
+  },
+  {
+	  header: 'Example usage:',
+	  content:'node app.js --ip localhost --port 3000 --message LOGAN IS HERE'
+  },
+  {
+    content: 'Project home: [underline]{https://github.com/sudoak/mock_tcp}'
+  }
+])
+console.log(usage)
 
 var ip = commandLineArgs(optionDefinitions).ip,
 	port = commandLineArgs(optionDefinitions).port,
